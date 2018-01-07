0f8af1f0cd15ec456f54dcf469baf5f538f2b984
LJN <lijn@zju.edu.cn>
2018-01-08 00:28:52 +0800

    members upadate

--- pre-members.py
+++ pre-members.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import LabelEncoder
 import time
 #%%
 path = 'F:\\DataMining\\musicRec\\'
-members = pd.read_csv(path + 'members.csv')
+members = pd.read_csv(path + 'members.csv', skiprows=lambda x: x > 1000)
 #%%
 columns = ['city', 'gender', 'registered_via']
 for column in columns:
@@ -16,7 +16,21 @@ for column in columns:
 
 members['registration_init_time'] = members['registration_init_time'].apply(
     lambda x: time.mktime(time.strptime(str(x), '%Y%m%d')))
+
 members['expiration_date'] = members['expiration_date'].apply(
     lambda x: time.mktime(time.strptime(str(x), '%Y%m%d')))
+#%%
+
+
+def check_age(x):
+    if x < 0:
+        return 0
+    elif x > 80:
+        return 80
+    else:
+        return x
+
+
+members['bd'] = members['bd'].apply(check_age)
 #%%
 members.to_csv(path + 'members_encode.csv', index=False)
