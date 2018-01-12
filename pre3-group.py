05c4f9dc41d93863c109fe047922b7e0248bdecb
LJN <lijn@zju.edu.cn>
2018-01-12 19:47:07 +0800

    grouping attempts.

--- pre3-group.py
+++ pre3-group.py
@@ -11,7 +11,7 @@ def skiprule(x): return x > 10000
 path = 'F:\\DataMining\\musicRec\\'
 coltype = {'genre_1st': np.int16, 'language': np.int16, 'artist_feat': np.int16, 'isrc_year': np.float32,
            'genre_2nd': np.int16, 'genre_3rd': np.int16, 'genre_count': np.int16}
-skiprule = None
+#skiprule = None
 
 train = pd.read_csv(path + 'train_all.csv', skiprows=skiprule)
 test = pd.read_csv(path + 'test_all.csv', skiprows=skiprule)
@@ -19,29 +19,45 @@ test = pd.read_csv(path + 'test_all.csv', skiprows=skiprule)
 print('loaded')
 #%%
 
+# Define function and colnames list.
+grouping_colns = ['msno']
+gruop_key_colns = []
 
-def get_digit(df, col)
-    return 10 ** (int(np.log10(df[col].max() + 1)) + 1)
 
-
-feature_digit = {}
-grouping_colns = []
-for coln in grouping_colns:
-    feature_digit[coln] = get_digit(concat, coln)
-
-#%%
+def get_digit(ser)
+    return 10 ** (int(np.log10(ser.max() + 1)) + 1)
 
 
 def combine_colname(name1, name2):
     return name1 + '_' + name2
 
 
-def combine_feature(df, cols1, cols2):
-    for col1 in cols1:
-        for col2 in cols2:
-            df[combine_colname(col1, col2)
-               ] = feature_digit[col1] * df[col1] + df[col2]
-    return
+def pair_feature(df, cols1, cols2):
+    '''
+    return a series pair the two IDs.
+    '''
+    series = feature_digit[col1] * df[col1] + df[col2]
+    return series
 
 
-np.log
+def get_group_mean(dftr, dfva, pair_tr):
+    map_mean = dftr.groupby(pair_tr).target.mean()
+
+    pair_coln = combine_colname(coln1, coln2)
+
+    for col1 in list1:
+        for col2 in list2:
+            group_tr = pair_feature(dftr, col1, col2)
+            map_mean = dftr.groupby(group_tr).target.mean()
+
+    combine_feature(dftr, list1, list2)
+    combine_feature(dfva, list1, list2)
+    map_mean = dftr[]
+
+
+#%%
+feature_digit = {}
+for coln in grouping_colns:
+    feature_digit[coln] = get_digit(train[coln].append(test[coln]))
+#%%
+train.info()
